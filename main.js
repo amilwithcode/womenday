13ef439e88981c1acf05dec6b89a62075feffac8
User <mywithcontact@gmail.com>
2025-03-08 14:57:28 +0400

    women day

---
+++ main.js
@@ -0,0 +1,35 @@
+const containerEl = document.querySelector('.container')
+const hbdEl = document.querySelector(".hbd")
+
+const word =[
+    "Happy Women's Day🎉",
+];
+
+const eightShape = [
+    "  ███  ",
+    " █   █ ",
+    " █   █ ",
+    "  ███  ",
+    " █   █ ",
+    " █   █ ",
+    "  ███  "
+];
+
+let index = 0;
+
+hbdEl.innerHTML = word.slice(0, index + 1).join("\n");
+function drawEight() {
+    containerEl.innerHTML = eightShape.slice(0, index + 1).join("\n");
+    index++;
+
+    if (index < eightShape.length) {
+        setTimeout(drawEight, 100);
+    } else {
+        setTimeout(() => {
+            index = 0;  // Yenidən başlat
+            drawEight(); // Sonsuz dövr yaradılır
+        }, 1000); // 1 saniyə gözləyib yenidən başlayır
+    }
+}
+
+drawEight();
